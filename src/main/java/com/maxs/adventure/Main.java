9c6e051aba8637373f122a4ed0c790d821137640
Jay Donaldson <j_donaldson12@hotmail.com>
2023-04-27 18:23:25 +0100

    feat: adding main and maven config

---
+++ src/main/java/com/maxs/adventure/Main.java
@@ -0,0 +1,20 @@
+package com.maxs.adventure;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This will be a main class that can be run in the terminal
+
+        // Simple Hello World will be first.
+
+        // Then read from terminal input using System.In and Buffered reader
+
+        // Explain the different data types i.e string, int , double
+
+        // Explain the first big-ish project (Hotel Booking system)
+
+        // git advice
+
+    }
+}
