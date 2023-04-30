dea864c4369acc41a7a1754b2c606518869a7b66
MaxS <mackeroony@hotmail.com>
2023-04-30 21:30:34 +0100

    chicken nuggets

--- src/main/java/com/maxs/adventure/Main.java
+++ src/main/java/com/maxs/adventure/Main.java
@@ -1,12 +1,22 @@
 package com.maxs.adventure;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception{
+        InputStreamReader r=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(r);
+
+        System.out.println("What is your name");
+        String nameOfUser=br.readLine();
+        System.out.println("What is up "+nameOfUser);
+        System.out.println("How are you");
         // This will be a main class that can be run in the terminal
 
-        // Simple Hello World will be first. cheesenuggets
+        // Simple Hello World will be first.
 
         // Then read from terminal input using System.In and Buffered reader
 
