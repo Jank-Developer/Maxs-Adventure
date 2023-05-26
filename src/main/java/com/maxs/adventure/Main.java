31820dfa3c2fe12b16ee551b9827945f6e1b67f1
Jay Donaldson <j_donaldson12@hotmail.com>
2023-05-26 18:30:07 +0100

    feat: well done

--- src/main/java/com/maxs/adventure/Main.java
+++ src/main/java/com/maxs/adventure/Main.java
@@ -1,24 +1,99 @@
 package com.maxs.adventure;
-
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        // This will be a main class that can be run in the terminal
+        // Initiate scanner and generate random number
+        Scanner scanner = new Scanner(System.in);
+        int number = 1 + (int) (100 * Math.random());
 
-        // Simple Hello World will be first.
+        //Establish total guesses
+        int guesses = 10;
+        int attempt, guess;
 
-        // Then read from terminal input using System.In and Buffered reader
+        //User instructions
+        System.out.println("A Number has been selected. ");
+        System.out.println("It is between 1 and 100. ");
+        System.out.println("You have " + guesses + " guesses. ");
 
-        // Explain the different data types i.e string, int , double
+        //Establish guessing loop (users interaction) attempts start at 0 and increment 1 each time a guess is made.
+        for (attempt = 0; attempt < guesses; attempt++){
 
-        // Show if/else, and switch
+            System.out.println("Take your guess.");
 
-        // show classes methods and interfaces
+            guess = scanner.nextInt();
 
-        // Explain the first big-ish project
+            if (number == guess) {
+                System.out.println("YOU ARE A WINNER!");
+                break;
+            }
+            else if (number > guess){
+                System.out.println("WRONG! " + guess +" is too LOW!");
+            }
+            else if (number < guess)
+                System.out.println("WRONG! " + guess +" is too HIGH!");
+        }
 
-        // git advice
+        /**
+         * LOOK HERE !!!!! if you move this out of the loop you can get rid of the && conditions
+         *
+         * HOWEVER, there was nothing wrong with your thinking I could clearly see your logic!!
+         * The mistakes with the logic worked and technically correct but were doing more than it needed
+         * this is all very expected and every beginner would do! I put your solution under commented out so
+         * you can take a look at what I mean.
+         *
+         * FIRST MISSION COMPLETE (o゜▽゜)o☆
+         */
+
+        if (attempt == guesses) {
+            System.out.println("STRRRIKE!. Try again!");
+            System.out.println("The answer was " + number );
+        }
+
 
     }
+
+//    import java.util.Scanner;
+//
+//    public class Main {
+//        public static void main(String[] args) {
+//// Initiate scanner and generate random number
+//            Scanner scanner = new Scanner(System.in);
+//            int number = 1 + (int) (100 * Math.random());
+//
+////Establish total guesses
+//            int guesses = 5;
+//            int attempt, guess;
+//
+////User instructions
+//            System.out.println("A Number has been selected. ");
+//            System.out.println("It is between 1 and 100. ");
+//            System.out.println("You have " + guesses + " guesses. ");
+//
+////Establish guessing loop (users interaction) attempts start at 0 and increment 1 each time a guess is made.
+//            for (attempt = 0; attempt < guesses; attempt++){
+//
+//                System.out.println("Take your guess.");
+//
+//                guess = scanner.nextInt();
+//
+//                if (number == guess) {
+//                    System.out.println("YOU ARE A WINNER!");
+//                    break;
+//                }
+//                else if (number > guess && attempt != guesses - 1){
+//                    System.out.println("WRONG! " + guess +" is too LOW!");
+//                }
+//                else if (number < guess && attempt != guesses - 1)
+//                    System.out.println("WRONG! " + guess +" is too HIGH!");
+//
+//            }
+//            if (guesses == attempt) {
+//
+//                System.out.println("STRRRIKE!. Try again!");
+//                System.out.println("The answer was " + number );
+//            }
+//
+//        }
+//    }
 }
